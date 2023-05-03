a9fda2e84e49ac05d6a3d21c2b5247420169ee34
wienerdev <matheuswiener9@gmail.com>
2023-05-03 20:29:36 -0300

    primeiro commit

---
+++ src/main/java/br/com/fiap/apipedidos/ApiPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.apipedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/apipedidos/ApiPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.apipedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
