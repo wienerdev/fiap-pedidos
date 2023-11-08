679a385d310379af1dd15e88dbf357b874a41115
Alisson Castro <t719132@f1rst.com.br>
2023-11-08 10:29:11 -0300

    adjusts health v3

--- src/main/java/br/com/fiap/api/pedidos/entrypoint/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/api/pedidos/entrypoint/controller/HealthCheckController.java
@@ -12,6 +12,6 @@ public class HealthCheckController {
 
     @GetMapping
     public ResponseEntity<String> getAppHealth() {
-        return new ResponseEntity<>("health OK V3", HttpStatus.OK);
+        return new ResponseEntity<>("health update to V3 - status: OK", HttpStatus.OK);
     }
 }
