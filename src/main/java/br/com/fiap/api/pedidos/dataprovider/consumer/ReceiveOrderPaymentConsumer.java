9f1dafb1e2e4832093209daa3a7ce999e3b37a1a
alexandre.dias <alexandrechavesdias@hotmail.com>
2024-03-19 16:28:53 -0300

    Retira logs e indenta

--- src/main/java/br/com/fiap/api/pedidos/dataprovider/consumer/ReceiveOrderPaymentConsumer.java
+++ src/main/java/br/com/fiap/api/pedidos/dataprovider/consumer/ReceiveOrderPaymentConsumer.java
@@ -9,6 +9,8 @@ import br.com.fiap.api.pedidos.dataprovider.repository.impl.OrderRepositoryImpl;
 import br.com.fiap.api.pedidos.dataprovider.repository.impl.SendCreatedOrderImpl;
 import br.com.fiap.api.pedidos.entrypoint.controller.dto.response.OrderSagaMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -19,6 +21,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class ReceiveOrderPaymentConsumer {
 
+    public static final Logger LOG = LogManager.getLogger(OrderMessage.class);
     private final OrderRepositoryImpl orderUseCase;
 
     private final SendCreatedOrderImpl sendCreatedOrder;
@@ -38,8 +41,8 @@ public class ReceiveOrderPaymentConsumer {
             orderUseCase.updateByOrderStatusAndOrderId(OrderStatusEnum.PREPARING, paymentMessage.getOrderId());
             var  orderMessage = OrderSagaMessage.toOrderMessage(order);
             Hibernate.initialize(orderMessage.getProductId());
-            System.out.println("Pedido pronto para iniciar o preparo.");
             sendCreatedOrder.send(orderMessage, OrderEvent.ORDER_STARTED);
+            LOG.info("CREATE ORDER TOPIC:[{}] AND MESSAGE:[{}]","tp-saga-payment", orderMessage);
         }
     }}
 
--- src/main/java/br/com/fiap/api/pedidos/dataprovider/repository/impl/SendCreatedOrderImpl.java
+++ src/main/java/br/com/fiap/api/pedidos/dataprovider/repository/impl/SendCreatedOrderImpl.java
@@ -2,17 +2,21 @@ package br.com.fiap.api.pedidos.dataprovider.repository.impl;
 
 
 import br.com.fiap.api.pedidos.dataprovider.configuration.message.OrderMessage;
-import br.com.fiap.api.pedidos.dataprovider.configuration.message.PaymentMessage;
 import br.com.fiap.api.pedidos.dataprovider.enumeration.OrderEvent;
 import br.com.fiap.api.pedidos.dataprovider.repository.SendCreatedOrderOutputPort;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@Slf4j
 public class SendCreatedOrderImpl implements SendCreatedOrderOutputPort {
 
+    public static final Logger LOG = LogManager.getLogger(OrderMessage.class);
     @Autowired
     private KafkaTemplate<String, OrderMessage> kafkaOrder;
 
@@ -21,6 +25,7 @@ public class SendCreatedOrderImpl implements SendCreatedOrderOutputPort {
         var orderMessage = new OrderMessage(order.getProductionId(), order.getClientCpf(), order.getOrderId(), order.getIsPaymentReceived(),
                 order.getOrderPrice(), order.getProductId(), event);
         kafkaOrder.send("tp-saga-orders", orderMessage);
+        LOG.info("CREATE ORDER TOPIC:[{}] AND MESSAGE:[{}]","tp-saga-orders", orderMessage);
     }
 
 
